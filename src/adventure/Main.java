ce3c4dabcec0a9a93c87e7a8bdaa147d4836ca03
aniasoja <annaj.soja@gmail.com>
2019-10-03 23:49:30 +0200

    First screen, with sliding on button click - to change for swipe up

---
+++ src/adventure/Main.java
@@ -0,0 +1,25 @@
+package adventure;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Underwater adventure");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
